6311b325ceb5ed75de4d439b76627b1b6487165a
Natalia <gon4arovanatalija@gmail.com>
2018-01-10 18:42:29 +0200

    playlist
    add new playlist button

--- app/components/playlist-sitebar-nav/playlist-sitebar-nav.js
+++ app/components/playlist-sitebar-nav/playlist-sitebar-nav.js
@@ -4,23 +4,31 @@ import React from 'react';
 
 
 export default class PlaylistSiteBarNav extends React.Component {
-    
+    constructor() {
+        super();
+        this.state = {
+            playlists: []
+        }  
+        this.createPlaylist = this.createPlaylist.bind(this);      
+    }
+
     createPlaylist(){
         console.log('3445');
-        this.playlists.push(
-            <li><a href="#">Cool trance music</a> </li>
-        )
+        const a= this.state.playlists.concat(<input type="text" autoFocus={true} className="playlist-input" placeholder="Untitled"></input>);
+        this.setState( {          
+            playlists: a
+        })
         }
-    render() {
-        var playlists =[];
+    render() {        
         return (
             <div className="site-bar-container">
                 <button className="add-new-playlist" onClick={this.createPlaylist}> Add new playlist </button>
                 <hr />
                 <ul>
-                    <li><a href="#">My songs</a></li>
-                    <li><a href="#">Cool trance music</a> </li>
-                    {playlists}
+                    {/* <li><a href="#">My songs</a></li>
+                    <li><a href="#">Cool trance music</a> </li> */}
+       
+       {this.state.playlists}
                 </ul>
             </div>
         );
